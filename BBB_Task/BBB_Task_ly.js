711af635900207a29bd7a1d472b793d4206bdcbc
luck-ying01 <77971626+luck-ying01@users.noreply.github.com>
2021-02-05 16:55:39 +0800

    Update BBB_Task_ly.js

--- BBB_Task/BBB_Task_ly.js
+++ BBB_Task/BBB_Task_ly.js
@@ -30,7 +30,7 @@ hostname = bububao.duoshoutuan.com,
 
 const $ = new Env('步步寶')
 let notice = ''
-let CookieVal = env.BBB_COOKIE_LY
+let CookieVal = process.env.BBB_COOKIE_LY
 
 if ($.isNode()) {
       console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
