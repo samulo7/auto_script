a0c639d871708b3e326a8315f7179b07ac598ee6
samulo7 <64634578+samulo7@users.noreply.github.com>
2021-03-24 18:40:52 +0800

    77
    777

--- BBB_Task/bububaoCOOKIE.js
+++ BBB_Task/bububaoCOOKIE.js
@@ -1,8 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let bububaotokenVal = `43F7876A4F29C53D5B3CA9D5D530283G1612665750
-                       1AF1971906164B677FF8A2B96530348G1612663771
-                       95230100FE2A362A15B4BF4E8530320G1612665258`
+let bububaotokenVal = `amk9dlta0q0tn68s0erp5hko75`
 
 
 
